064b7280a29134abe6e5da391fcaf655c45c75c9
rivumitter <rivumitter@gmail.com>
2024-09-01 15:28:50 +0530

    auth server

---
+++ src/main/java/com/rivu/authserver/config/AuthserverConfig.java
@@ -0,0 +1,21 @@
+package com.rivu.authserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class AuthserverConfig {
+
+    @Bean
+    UserDetailsService inmemoryUserDetailsManger() {
+        var userBuilder = User.builder();
+
+        return new InMemoryUserDetailsManager(
+                userBuilder.username("rivu").password("{noop}pwd").build(),
+                userBuilder.username("rima").password("{noop}pwd").build()
+        );
+    }
+}
